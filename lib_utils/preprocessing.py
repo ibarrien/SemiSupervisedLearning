13723bc0aea167585ed607cdc401d8028aa49d37
ivbarrie <ivbarrie@microsoft.com>
2021-12-20 21:08:28 -0800

    rollback

--- lib_utils/preprocessing.py
+++ lib_utils/preprocessing.py
@@ -56,7 +56,7 @@ def is_english(text: str, english_vocab: List[str]) -> str:
     return english_words_text
 
 
-def _process_text(text: str, tokens_to_remove: List[str],
+def process_text(text: str, tokens_to_remove: List[str],
                  english_vocab: List[str], stemmer=None) -> str:
     """Basic text processing."""
     # filtered.translate(str.maketrans('', '', string.punctuation))
@@ -82,11 +82,7 @@ class TextPreProcessor:
         remove_fields (Tuple(str)): fields to remove from 20 Newsgroup
 
     Notes:
-        labeled_train_data_sample: sample from full train data (can vary in experiments)
-        unlabeled_data: typically fixed over experiments
-        test_data: fixed over experiments
-
-        To avoid overfitting on 20NewsGroups, recommended to set:
+        To avoid overfitting, recommended to set:
         remove_fields=('headers', 'footers', 'quotes')
     """
     def __init__(self, tokens_to_remove, english_vocab=None,
@@ -107,9 +103,9 @@ class TextPreProcessor:
         self.remove_zero_vocab_docs = remove_zero_vocab_docs
         self.n_unlabeled_train_samples = n_unlabeled_train_samples
         self.n_labeled_train_samples = n_labeled_train_samples
-        self.labeled_train_data_sample = np.array([])  # raw train data sample
-        self.labeled_train_sample_count_data = np.array([])  # count vectorizer of labeled train data
-        self.train_sample_label_vals = np.array([])  # label vals of training data
+        self.train_data = [""]
+        self.train_label_vals = [0]
+        self.train_count_data = [0]
         self.test_data = [""]
         self.test_label_vals = [0]
         self.test_count_data = [0]
@@ -130,83 +126,62 @@ class TextPreProcessor:
 
     def fetch_train_raw_data(self):
         """Fetch raw train data; this method should only be called once"""
-        self.full_train_bunch = self.load_data(subset='train')  # 11,314  total samples
-        self.full_train_label_names = self.full_train_bunch[self.label_names_key]  # List[str], len = 20
-        self.full_train_data = np.array(self.full_train_bunch.data)  # np.ndarray[str]
-        self.full_train_label_vals = np.array(self.full_train_bunch[self.label_vals_key])  # List[str], len = len(self.train_data)
+        self.train_bunch = self.load_data(subset='train')  # 11,314  total samples
+        self.train_label_names = self.train_bunch[self.label_names_key]  # List[str], len = 20
+        self.train_data = np.array(self.train_bunch.data) # np.ndarray[str]
+        self.train_label_vals = np.array(self.train_bunch[self.label_vals_key]) # List[str], len = len(self.train_data)
 
-    def set_static_raw_unlabeled_data(self):
-        """Fix sample of 'unlabaled' data from full train."""
-        self.unlabeled_train_indices = np.random.choice(a=range(len(self.full_train_data)),
+    def set_static_unlabeled_data(self):
+        """Fix sample of train raw data; used as unlabeled data in semi-supervised learning."""
+        self.unlabeled_train_indices = np.random.choice(a=range(len(self.train_data)),
                                                         size=self.n_unlabeled_train_samples,
                                                         replace=False)
-        self.unlabeled_train_data = self.full_train_data[self.unlabeled_train_indices]
+        self.unlabeled_train_data = self.train_data[self.unlabeled_train_indices]
 
-    def set_sample_raw_train_data(self):
-        """Select sample of train data; run once per eval iteration on varying train size."""
-        assert len(self.unlabeled_train_data) > 0, "Set unlabeled data before setting current labeled train sample."
-        self.avail_train_indices = list(set(range(len(self.full_train_data))) - set(self.unlabeled_train_indices))
-        if not self.avail_train_indices:
-            raise Exception("Sampling zero labeled train examples; decrease unlabeled sample size.")
-        print('Setting %d labeled train samples' % self.n_labeled_train_samples)
-        self.labeled_train_sample_indices = np.random.choice(a=self.avail_train_indices,
-                                                      size=self.n_labeled_train_samples,
-                                                      replace=False)
+    def set_sample_train_data(self):
+        """Select sample of train data; used as labeled data in semi-supervised learning."""
+        self.avail_train_indices = set(range(len(self.train_data))) - set(self.unlabeled_train_indices)
+        n_original_train_samples = len(self.train_data)
+        n_train_samples = int(self.train_pct_subsample * n_original_train_samples)
+        print('selecting %d train samples from %d' %
+              (n_train_samples, n_original_train_samples))
+        # Shuffled train data on load by default
+        self.train_data = self.train_data[: n_train_samples]
+        self.train_label_vals = self.train_label_vals[: n_train_samples]
 
-        self.labeled_train_data_sample = self.full_train_data[self.labeled_train_sample_indices]
-        self.train_sample_label_vals = self.full_train_label_vals[self.labeled_train_sample_indices]
 
-    def set_static_raw_test_data(self):
+    def set_static_test_raw_data(self):
         """Fetch and fix standard 20 NewsGroup Test Data; should only be called once"""
-        full_test_bunch = self.load_data(subset='test')
-        self.full_test_data = np.array(full_test_bunch.data)  # List[str]
-        self.full_test_label_vals = np.array(full_test_bunch[self.label_vals_key])
+        test_bunch = self.load_data(subset='test')
+        self.test_data = np.array(test_bunch.data)  # List[str]
 
-    def process_text(self, text_list: np.ndarray) -> List[str]:
+    def process_text(self, text_list: List[str]) -> List[str]:
         """Apply basic text pre-processing to loaded data."""
-        print('received text_list:', text_list.shape, len(text_list))
-        x_proc = [_process_text(x, tokens_to_remove=self.tokens_to_remove, english_vocab=self.english_vocab)
+        x_proc = [process_text(x, tokens_to_remove=self.tokens_to_remove, english_vocab=self.english_vocab)
                   for x in text_list]  # list of sentences
         return x_proc
 
     def preprocess_data_to_array(self, subset: str = 'train') -> np.ndarray:
         """Preprocess text to 2-d array of word counts via count vectorization.
-
-        Params
-        ------
-        subset (str): type of dataset wrt building generative model or inference
-
-        Returns
-        -------
-        data_vect_array (np.ndarray): representation of documents as word counts
-
-        Notes:
-        data_vect_array: Rows = document index; Columns = word count index
+        Rows: document index; Columns: word count index
         """
-        data = np.array([])
         if subset == 'train':
-            # use count vectorizer to fit and transform on labeled train data
+            # use count vectorizer to fit and transform
             self.count_vectorizer = CountVectorizer()
-            if len(self.labeled_train_data_sample) == 0:
+            if not self.train_data or len(self.train_data) < 2:
                 raise Exception("(raw) train data not set; run set_train_raw_data()")
-            data = self.labeled_train_data_sample
+            data = self.train_data
             x_proc = self.process_text(text_list=data)
             x_proc_vect = self.count_vectorizer.fit_transform(x_proc)  # scipy.sparse.csr.csr_matrix
             self.vocab = self.count_vectorizer.get_feature_names()  # List[str]
         else:
-            assert self.count_vectorizer, "Count vectorizer not set, run subset='train' first"
             # use count_vectorizer transform only, do not fit
             if subset == 'unlabeled':
-                data = self.unlabeled_train_data
-                print('got data=unlabeled, shape=', data.shape)
-            elif subset == 'test':
-                data = self.full_test_data
-            else:
-                raise Exception("Preprocessing data type must be 'train', 'unlabeled', or 'test'")
-            if len(data) == 0:
+                data = self.unlabeled_data
+            if subset == 'test':
+                data = self.test_data
+            if not data or len(data) < 2:
                 raise Exception("(raw) %s data not set; run\n set_test_raw_data()" % subset)
-            print('data shape = ', data.shape)
-            print('data type = ', type(data))
             x_proc = self.process_text(text_list=data)
             x_proc_vect = self.count_vectorizer.transform(x_proc)
         data_vect_array = x_proc_vect.toarray()
@@ -214,50 +189,34 @@ class TextPreProcessor:
 
     @staticmethod
     def remove_zero_count_docs(doc_count_data: np.ndarray, vocab_axis: int = 1) -> Tuple[np.ndarray]:
-        """Remove zero count doc vectors wrt *preprocessed vocab* and keep their labels.
-
-        Notes: only applied labeled train and labeled test data, not unlabeled dataset.
-        """
+        """Remove zero count doc vectors wrt *preprocessed vocab* and keep their labels."""
         count_sums = np.sum(doc_count_data, axis=vocab_axis)
         mask = count_sums > 0  # use original indices for label val retrieval
         nonzero_doc_data = doc_count_data[mask]
         return nonzero_doc_data, mask
 
-    def set_labeled_train_count_data(self):
-        """Set np.ndarray of bag of words for (labeled) train set; rows: docs, cols: word counts.
-
-        Notes:
-            Since unlabeled data is random, this method does not apply to unlabeled dataset
-        """
-        self.labeled_train_sample_count_data = self.preprocess_data_to_array(subset="train")
+    def set_train_count_data(self):
+        """Set np.ndarray of bag of words for train set; rows: docs, cols: word counts."""
+        self.train_count_data = self.preprocess_data_to_array(subset='train')
         if self.remove_zero_vocab_docs:
-            print("Removing zero vocab docs from labeled train sample.")
-            self.labeled_train_sample_count_data, mask =\
-                self.remove_zero_count_docs(doc_count_data=self.labeled_train_sample_count_data)
-            original_labeled_train_size = len(self.labeled_train_sample_indices)
-            self.train_sample_label_vals = self.train_sample_label_vals[mask]
+            self.train_count_data, mask = self.remove_zero_count_docs(doc_count_data=self.train_count_data)
+            original_train_size = len(self.train_label_vals)
+            self.train_label_vals = self.train_label_vals[mask]
             print('After zero count doc removal:\n Kept %d samples from original %d train'
-                  % (len(self.train_sample_label_vals), original_labeled_train_size))
-        return None
-
-    # TODO: set flag on whether to remove zero count (wrt labeled train) docs from unlabeled/test
-    def set_unlabeled_count_data(self):
-        """Set np.ndarray of bag of words for unlabeled set; rows: docs, cols: word counts."""
-        self.unlabeled_count_data = self.preprocess_data_to_array(subset='unlabeled')
-        # self.unlabeled_count_data = self.remove_zero_count_docs(doc_count_data=self.unlabeled_count_data)
+                  % (len(self.train_label_vals), original_train_size))
         return None
 
     def set_test_count_data(self):
         """Set np.ndarray of bag of words for test set; rows: docs, cols: word counts."""
         self.test_count_data = self.preprocess_data_to_array(subset='test')
-        # self.test_count_data = self.remove_zero_count_docs(doc_count_data=self.test_count_data)
+        self.test_count_data = self.remove_zero_count_docs(doc_count_data=self.train_test_data)
         return None
 
     def get_doc_lens(self):
         """Compute constant doc length via max len of train set."""
-        self.train_doc_lens = np.sum(self.labeled_train_sample_count_data, axis=self.vocab_axis)
-        self.max_doc_len = np.max(self.labeled_train_sample_count_data)
-        self.med_doc_len = np.median(self.labeled_train_sample_count_data)
+        self.train_doc_lens = np.sum(self.train_count_data, axis=self.vocab_axis)
+        self.max_doc_len = np.max(self.train_doc_lens)
+        self.med_doc_len = np.median(self.train_doc_lens)
         return None
 
     @staticmethod
