eb4148b83192365211a23632e35a651c8fbe0ccf
mannyk <man962@yahoo.com>
2022-01-25 07:05:38 -0800

    update unit test for config.

--- src/t.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-from pathlib import Path, PurePosixPath
-
-from lib_utils import nltkconfig
-
-reporoot = nltkconfig.getRepoRoot()
-print(reporoot)
-print(nltkconfig.getDataFolder())
--- src/t_nltkconfig.py
+++ src/t_nltkconfig.py
@@ -1,12 +1,8 @@
-import os, sys
-from pathlib import Path, PurePosixPath
-
 from lib_utils import projconfig
 from lib_utils import nltkconfig
 
 if __name__ == '__main__':
 	reporoot = projconfig.getRepoRoot()
-	print(f"reporoot={reporoot}")
-	datafolder = projconfig.getDataFolder()
-	print(f"datafolder={datafolder}")
-	print(nltkconfig.getDataFolder())
+	print(f"reporoot {reporoot}")
+	print(f"data {projconfig.getDataFolder()}")
+	print(f"nltkconfig.getDataFolder() {nltkconfig.getDataFolder()}")
